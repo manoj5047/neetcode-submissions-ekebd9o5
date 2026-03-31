12d57fdca17dbe619ecb4cb04579665c2e5f83b6
SamSuga <24776150+manoj5047@users.noreply.github.com>
2026-03-31 17:47:35 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        val sca = s.toCharArray()
+        val tca = t.toCharArray()
+        sca.sort()
+        tca.sort()
+        return sca.toList().equals(tca.toList())
+    }
+}
